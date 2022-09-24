d360383d241afa6a4d7d90eba21f01924b965082
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-09-24 16:29:19 -0300

    Feat pagination with "previous page" and "next page" functions
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,32 @@
 <script>
 
   let characters = [];
+  let page = 1;
 
   async function getLoadCharactersAPIClient(){
-    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const response = await fetch('https://rickandmortyapi.com/api/character?page=' + page);
     const {results} = await response.json()
     characters = results;
   }
-
   getLoadCharactersAPIClient();
+
+  function nextPage(){
+    page++;
+    getLoadCharactersAPIClient();
+  }
+
+  function previousPage(){
+    page--;
+    getLoadCharactersAPIClient();
+  }
 </script>
 
 <main>
   <h1>Rick and Morty Svelte</h1>
 
   <div>
-    <button>Previous</button>
-    <button>Next</button>
+    <button on:click={previousPage} disabled={page <= 1}>Previous</button>
+    <button on:click={nextPage}>Next</button>
   </div>
 
   {#each characters as character}
