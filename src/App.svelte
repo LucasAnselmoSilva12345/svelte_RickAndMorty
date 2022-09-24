b7e2dd9498b26ace7271068139da21d7856ec870
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-09-24 16:17:24 -0300

    Feat API connection and data imports to front-end
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,29 @@
 <script>
 
+  let characters = [];
+
+  async function getLoadCharactersAPIClient(){
+    const response = await fetch('https://rickandmortyapi.com/api/character');
+    const {results} = await response.json()
+    characters = results;
+  }
+
+  getLoadCharactersAPIClient();
 </script>
 
 <main>
-  <h1>Hello world</h1>
+  <h1>Rick and Morty Svelte</h1>
+
+  <div>
+    <button>Previous</button>
+    <button>Next</button>
+  </div>
+
+  {#each characters as character}
+    <div>
+      <img src={character.image} alt={character.name}>
+      <h2>{character.name}</h2>
+      <h3>{character.species}</h3>
+    </div>
+  {/each}
 </main>
