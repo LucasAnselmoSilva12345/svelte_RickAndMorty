c5d6c067b116dd2128e41cac0f9aa3df5478c934
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-09-24 17:46:45 -0300

    Feat styles using TailwindCss
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,8 @@
     const response = await fetch('https://rickandmortyapi.com/api/character?page=' + page);
     const {results} = await response.json()
     characters = results;
+
+    console.log(characters)
   }
   getLoadCharactersAPIClient();
 
@@ -21,15 +23,24 @@
   }
 </script>
 
-<main>
-  <h1>Rick and Morty Svelte</h1>
-
-  <div>
-    <button on:click={previousPage} disabled={page <= 1}>Previous</button>
-    <button on:click={nextPage}>Next</button>
+<main class="text-gray-400 bg-gray-900 body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Our Team</h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+    </div>
+    <div class="flex justify-between mb-4">
+      <button on:click={previousPage} disabled={page <= 1} class="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
+        Prev
+      </button>
+      <button on:click={nextPage} class="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded">
+        Next
+      </button>
+    </div>
+    <div class="flex flex-wrap -m-2">
+      {#each characters as character}
+          <Character characterDataFromRequest={character}/>
+        {/each}
+    </div>
   </div>
-
-  {#each characters as character}
-    <Character characterDataFromRequest={character}/>
-  {/each}
 </main>
--- src/lib/Character.svelte
+++ src/lib/Character.svelte
@@ -2,8 +2,12 @@
   export let characterDataFromRequest; // this variable receive the "export" value  then yours values comes from another file
 </script>
 
-<div>
-  <img src={characterDataFromRequest.image} alt={characterDataFromRequest.name}>
-  <h2>{characterDataFromRequest.name}</h2>
-  <h3>{characterDataFromRequest.species}</h3>
+<div class="p-2 lg:w-1/3 md:w-1/2 w-full">
+  <div class="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+    <img class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={characterDataFromRequest.image} alt={characterDataFromRequest.name}>
+    <div class="flex-grow">
+      <h2 class="text-white title-font font-medium">{characterDataFromRequest.name}</h2>
+      <p class="text-gray-600">{characterDataFromRequest.species}</p>
+    </div>
+  </div>
 </div>
